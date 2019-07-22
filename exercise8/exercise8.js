ac9c9da08beb3a50f11340f5a6caa5dd1c786fe2
mohammad-talib <mohammad.h.alhety@gmail.com>
2019-07-22 20:38:19 +0300

    exercise-dom

---
+++ exercise8/exercise8.js
@@ -0,0 +1,26 @@
+function initGrid() {
+    // collect colors in an array
+    var colors = [];
+    var range = ["00", "33", "66", "99", "cc", "ff"];
+
+    for (var r = 0; r < range.length; r++) {
+        for (var g = 0; g < range.length; g++) {
+            for (var b = 0; b < range.length; b++) {
+                colors.push("#" + range[r] + range[g] + range[b]);
+            }
+        }
+    }
+
+    var color = document.getElementById("colors");
+
+    for (var i = 0; i < colors.length; i++) {
+        var great = document.createElement("div");
+        great.className = "choice";
+        great.style.backgroundColor = colors[i];
+        color.appendChild(great);
+    }
+}
+
+window.onload = function () {
+    initGrid();
+}
